a99206aa3e6c8dfa9a67be54fc06f9195d529682
Markus <markus.sussitz@gmail.com>
2018-01-11 23:48:37 +0100

    Added input checks.

--- src/extension.ts
+++ src/extension.ts
@@ -19,27 +19,68 @@ export function activate(context: vscode.ExtensionContext) {
         startCreateFileFromTemplate(clickedFolderPath)
     });
 
+    let disposable2 = vscode.commands.registerTextEditorCommand("extension.importTemplateToFile", (editor, edit, file) => {
+
+        console.log("Das ist ein Test");
+    });
+
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
 }
 
 function startCreateFileFromTemplate(clickedFolder: string) {
     let workspaceFolder = getWorkSpaceFolder(clickedFolder);
-    let templates = getTemplateFiles(workspaceFolder.uri.path);
 
-    vscode.window.showQuickPick(templates.map(x => x.name)).then(item => {
+    chooseTemplate(workspaceFolder.uri.path)
+        .then(templatePath => {
+            if (!templatePath) {
+                return;
+            }
+
+            createFileFromTemplate(clickedFolder, templatePath)
+        });
+}
+
+function chooseTemplate(workspaceFolder: string): Thenable<string> {
+    let templates = getTemplateFiles(workspaceFolder);
+    return vscode.window.showQuickPick(templates.map(x => x.name)).then(item => {
+        if (!item) {
+            return;
+        }
+
         let selectedTemplate = templates.filter(x => x.name === item)[0];
-        createFileFromTemplate(clickedFolder, selectedTemplate.path);
+        return selectedTemplate.path;
     });
 }
 
-function createFileFromTemplate(targetfolder: string, templatePath: string) {
-    vscode.window.showInputBox({ placeHolder: 'Please enter a filename' }).then(filename => {
+function chooseFilename(templatePath: string): Thenable<string> {
+    return vscode.window.showInputBox({ placeHolder: 'Please enter a filename' }).then(filename => {
+        if (typeof (filename) === 'undefined') {
+            return;
+        }
+
+        if (!filename) {
+            vscode.window.showErrorMessage('Filename cannot bet empty.');
+            return;
+        }
+
         if (!path.extname(filename)) {
             filename = `${filename}${path.extname(templatePath)}`;
         }
 
+        return filename;
+    });
+}
+
+function createFileFromTemplate(targetfolder: string, templatePath: string) {
+    chooseFilename(templatePath).then(filename => {
+        if (!filename) {
+            return;
+        }
+
         var targetFile = path.join(targetfolder, filename);
-        var templateString = fs.readFileSync(templatePath, { encoding: 'utf8' });
+        var templateString = loadTemplate(templatePath);
+
         var writer = fs.createWriteStream(targetFile);
 
         writer.on('finish', () => {
@@ -52,15 +93,19 @@ function createFileFromTemplate(targetfolder: string, templatePath: string) {
     });
 }
 
+function loadTemplate(templatePath: string): string {
+    return fs.readFileSync(templatePath, { encoding: 'utf8' });
+}
+
 function getWorkSpaceFolder(folder: string) {
     return vscode.workspace.getWorkspaceFolder(vscode.Uri.file(folder));
 }
 
 function getTemplateFiles(workspaceDir: string): { name: string, path: string }[] {
-    let templates;
-
     let templateFolder = path.join(workspaceDir, '.vscode', 'templates');
     let templateFolderExists = fs.existsSync(templateFolder);
+
+    let templates;
 
     if (templateFolderExists) {
         templates = fs.readdirSync(templateFolder).map(file => ({ name: file, path: path.join(templateFolder, file) }));
